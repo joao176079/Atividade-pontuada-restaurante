f6ddd96fe3108fc253295345865fc86d1e6f4e87
Plataforma de Telemedicina com Acessibilidade Avançada <joao176069@gmail.com>
2024-09-23 19:23:58 +0000

    Salvar codigo 2

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -11,6 +11,25 @@ Nome completo dos componentes.
 import os
 
 # Limpa o terminal
-os.system("cls || clear") 
+os.system("cls || clear")
+
+print ("Bem vindo ao Gourmet Tech Restaurant!")
+
+nome = input ("Digite o seu nome : ")
+
+print ("""
+     Cardápio :
+       
+    1 - Pão com ovo = 4.00 R$
+    2 - Strogonoff " Acompanha-se arroz e batata palha " = 28.00
+    3 - Feijoada " Acompanha-se arroz e salada " = 24.00
+    4 - Bife à milanesa " Acompanha-se arroz e salada " = 28.00
+    5 - Hamburguer Artesanal " Acompanha fritas " = 30.00
+    6 - Filé com fritas " Acompanha-se arroz " = 26.00
+    7 - Salmão grelhado " Acompanha-se arroz e salada " = 32.00
+       
+    
+       """)
+
 
 
